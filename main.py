45ad2eeebcb58e84e6c564480c86f0fb11bfe59e
Alejandro <alejmac2508@gmail.com>
2024-11-19 01:38:13 -0600

    se agregaron cambios de color a tira de materias

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main(page: Page):
             "/first": lambda: FirstView(page)
         }
         
-        view_function = routes.get(page.route, routes["/schedule"])
+        view_function = routes.get(page.route, routes["/teachers"])
         page.views.append(view_function())   
  
         page.update()
